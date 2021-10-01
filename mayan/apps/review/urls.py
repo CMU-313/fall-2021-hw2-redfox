15aa073f9de2b3091017bf1a8952b67ffd4a9e4c
Varshaa <vsekarja@andrew.cmu.edu>
2021-10-01 12:47:14 -0400

    code structure

--- mayan/apps/review/urls.py
+++ mayan/apps/review/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    TagCreateView
+}
+
 
 urlpatterns = [
     path('', views.index, name='review')
+    url(
+        regex=r'^tags/create/$', name='tag_create',
+        view=TagCreateView.as_view()
+    )
 ]
--- mayan/apps/review/views.py
+++ mayan/apps/review/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
 
+from mayan.apps.views.generics import (
+    MultipleObjectFormActionView, MultipleObjectConfirmActionView,
+    SingleObjectCreateView, SingleObjectEditView, SingleObjectListView
+)
+from mayan.apps.views.mixins import ExternalObjectViewMixin
+from .forms import TagMultipleSelectionForm
+
 def index(request):
     return HttpResponse("review")
+
+class TagCreateView(SingleObjectCreateView):
+    extra_context = {'title': _('Create tag')}
+    fields = ('label', 'color')
+    model = Tag
+    post_action_redirect = reverse_lazy(viewname='tags:tag_list')
+    view_permission = permission_tag_create
+
+    def get_instance_extra_data(self):
+        return {
+            '_event_actor': self.request.user
+        }
